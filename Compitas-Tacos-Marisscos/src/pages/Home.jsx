7067fc7fdb6214277dd65b278dbd1c54e93748d3
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2025-02-17 19:01:45 -0700

    added photos to home.jsx

--- Compitas-Tacos-Marisscos/src/pages/Home.jsx
+++ Compitas-Tacos-Marisscos/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import '../styles/Home.css';
 import Truck from '../public/Truck.svg';
-
-
+import aguaChileBowl from '../public/bowl.png';
+import shrimpTaco from '../public/shrimp-taco3.jpg';
+import shrimpBowl from '../public/shrimp-bowl.png';
+import cocktail from '../public/cup.png';
+import chips from '../public/chips.jpg';
+import charola1 from '../public/charola1.png';
+import charola2 from '../public/charola2.png';
+import avoBowl from '../public/avo-bowl.png';
 
 function Home() {
     const [name, setName] = useState('');
@@ -30,22 +36,27 @@ function Home() {
                                 <li>COCTELES</li>
                                 <li>CHAROLAS</li>
                                 <li>DRINKS</li>
+                                <li>CONTACT US FOR LARGE ORDERS, CHAROLAS OR CATERING</li>
                             </ul>
                         </span>
-                        - CONTACT US FOR LARGE ORDERS, CHAROLAS OR CATERING</marquee>
+                        </marquee>
                 </div>
 
             <div alt="picture of Compitas's Food Truck" className="the-truck" >
                 <img src={Truck} alt="Truck" />
             </div>
 
+            <div className="photo-row">
+                    <img src={aguaChileBowl} alt="Aguachile Bowl" />
+                    <img src={avoBowl} alt="Avo Bowl" />
+                    <img src={cocktail} alt="Cocktail" />
+                    <img src={charola1} alt="Charola" />
+                    <img src={charola2} alt="Charola" />
+            </div>
+
             <div className="contact-us">
                 <h1>Contact Us</h1>
                 <p>Feel free to contact us with any questions or comments or for a free quote.</p>
-            </div>
-
-            <div>
-                
             </div>
 
             <div >
